b1d025c1b8fef1d6e0c83f087ca979850abc9912
andiputraw <andiputraw925@gmail.com>
2023-08-22 18:27:28 +0700

    added questions

--- #7266/advanced.rs
+++ #7266/advanced.rs
@@ -1,6 +1,14 @@
 /// Ada dua jawaban untuk challenge ini
 /// Pertama. melalui pendekatan imperative
 /// dan kedua. menggunakan pendekatan functional/declarative
+/// 
+/// Pertanyaan
+/// 5 4 3 2 1
+/// 4 3 2 1
+/// 3 2 1
+/// 2 1
+/// 1
+/// Write a program to use for loop to print the following reverse number pattern
 
 use std::time::Instant;
 
--- #7266/basic.py
+++ #7266/basic.py
@@ -1,3 +1,23 @@
+# Diselesaikan dengan pendekatan Imperative dan didesain agar bisa di maintain dengan mudah (kalaupun sebenernya gak perlu jgua sih)
+
+# Pertanyaan
+
+# Pada suatu hari udin dan nanang sedang mencoba membandingkan BMI (Indeks Massa Tubuh) mereka, yang dihitung menggunakan rumus: BMI = massa / tinggi ** 2 = massa / (tinggi * tinggi) (massa dalam kg dan tinggi dalam meter).
+
+# Tugas Anda, bantulah udin dan nanang menghitung BMI mereka mengikuti langkah langkah berikut ini:
+
+# 1. Simpan massa dan tinggi udin dan nanang dalam sebuah variabel.
+
+# 2. Hitunglahﾠ kedua BMI mereka menggunakan rumus (kamu bahkan dapat mengimplementasikan kedua versi rumus diatas).
+
+# 3. Buat variabel dengan output Boolean yang berisi informasi apakah udin memiliki BMI yang lebih tinggi daripada nanang.
+
+# 4. Tampilkan keluaran yang bagus di konsol, yang menyatakan siapa yang memiliki BMI yang lebih tinggi.
+
+# 5. Pesannya dapat berupa “BMI Udin lebih tinggi dari Nanang!” atau sebaliknya.
+
+# 6. Gunakan konsep template literal untuk menyertakan nilai-nilai BMI dalam keluaran. Contoh: “BMI Udin (28,3) lebih tinggi dari Nanang (23,9)!”
+
 import time
 
 class Person:
